b31a5b5660122f7276c0735bbb32527c1d6b0a07
David Skeen <skeenda@oregonstate.edu>
2019-03-11 09:46:46 -0700

    Added Connection Vars for my DB

---
+++ connectvars.php
@@ -0,0 +1,9 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_USER', 'cs440_skeenda');
+  define('DB_PASSWORD', '5895');
+  define('DB_NAME', 'cs440_skeenda');
+  define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs440_skeenda');
+?>
+
